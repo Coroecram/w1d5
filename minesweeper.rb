93801a8bf762e9b391bf6af344325df732e73497
Michael Budnick <michaeldbudnick@gmail.com>
2015-10-02 12:07:02 -0400

    Create Tile and Board class and neighbors method.

--- minesweeper.rb
+++ minesweeper.rb
@@ -2,12 +2,29 @@
 
 class Tile
 
-  def initialize(mined = nil)
+  attr_accessor :flagged, :revealed
+  attr_reader :mined, :board
+
+  NEIGHBOR = [[1, 0], [0, 1], [-1, 0], [0, -1], [1, 1], [-1, 1], [1, -1], [-1, -1]]
+
+  def initialize(mined = nil, board)
     @mined = mined
     @flagged = nil
     @revealed = false
+    @board = board
   end
 
-  
+  def reveal
+    self.revealed = true
+  end
+
+  def neighbors(pos)
+    neighbors = []
+    NEIGHBOR.each do |i|
+      neighbors << [pos[0] + i[0], pos[1] + i[1]]
+    end
+
+    neighbors
+  end
 
 end
